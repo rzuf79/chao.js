9ab06fb3a7fc6308d9f3e3b384015cf3c97f9b91
rzuf <rzuf79@gmail.com>
2021-01-13 17:49:15 +0100

    Built-in font is now a custom one; made helper methods add created entities to whatever parents

--- Template/StateGame.js
+++ Template/StateGame.js
@@ -1,17 +1,15 @@
 function StateGame(){
 	
 	this.create = function(){
-		var newSprite = chao.helpers.createSprite("Sticker", "sticker");
-		this.add(newSprite.entity);
+		var newSprite = chao.helpers.createSprite(this, "Sticker", "sticker");
 		newSprite.entity.alignToParent();
 		newSprite.entity.addComponent(new ComponentTemplate(this));
 
 		var introText = "chao.js is working!\nversion " + chao.VERSION;
-		var newText = chao.helpers.createText("Version Text", newSprite.entity.width/2, 0, chao.font, introText, 23);
+		var newText = chao.helpers.createText(newSprite.entity, "Version Text", newSprite.entity.width/2, 0, chao.font, introText, 16);
+		newText.entity.alignToParent(0.5, 1.0, 0.5, 0.0, 10);
 		newText.color = chao.colorCodes[7];
 		newText.align = "center";
-		newSprite.entity.add(newText.entity);
-		newText.entity.alignToParent(0.5, 1.0, 0.5, 0.0, 10);
 	}
 
 	this.resize = function(){
